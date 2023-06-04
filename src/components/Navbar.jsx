32c2a5c4316a5f2b0d0ecebc80d97b90255d8a0f
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-06-04 15:20:26 +0530

    images changed and toast added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
 import react from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Navbar = () => {
+
+    const handleClick = () => {
+        toast.error("Coming Soon...", { autoClose: 3000 });
+    }
+
     return (
+        <>
         <div className='nav-bar' id='nav'>
             <a href="#" style={{textDecoration : "none"}}>
             <h1><span className='nav-name'> <img src="./favLogo.png" alt="logo-nav" height={32} width={35} style={{marginBottom: "-5px", marginRight: "7px",paddingTop: "5px", marginTop: "5px"}}/> Patient <span>47</span></span> </h1>
             </a>
-            <h2><span className='nav-buy'>BUY NOW</span></h2>
+            <h2 ><span className='nav-buy' onClick={handleClick}>BUY NOW</span></h2>
         </div>
+        <ToastContainer
+      position="bottom-right"
+      />
+        </>
     )
 }
 
--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -8,28 +8,28 @@ export default function Overwiew() {
             <div className="boxes-overview">
             <h1 style={{textAlign : "center"}}> <span> Game Overview </span></h1>
                <Card 
-                img="https://static1.cbrimages.com/wordpress/wp-content/uploads/2023/04/one-piece-zoro-s-10-best-moves-ranked-according-to-strength.jpg"
+                img="./3.png"
                 heading="My Dummy Text"
                 para1="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro blanditiis cum alias voluptate fugiat ea, repellat omnis sequi accusamus officia nemo praesentium quibusdam quo illum sint id explicabo repellendus, quia eaque ex? Maxime vitae officia aliquam, nisi perspiciatis id quasi corrupti, perferendis ducimus vero ipsa!lorem10 Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, tenetur!"
                 para2="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quasi unde nesciunt adipisci numquam iure explicabo, suscipit distinctio maxime consectetur corrupti voluptates, aliquid, sit consequatur!"
                 />
 
                <Card2
-               img="https://i.ytimg.com/vi/uAbF5ROUeVo/maxresdefault.jpg"
+               img="./2.png"
                heading="My Dummy Text"
                para1="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro blanditiis cum alias voluptate fugiat ea, repellat omnis sequi accusamus officia nemo praesentium quibusdam quo illum sint id explicabo repellendus, quia eaque ex? Maxime vitae officia aliquam, nisi perspiciatis id quasi corrupti, perferendis ducimus vero ipsa!lorem10 Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, tenetur!"
                para2="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quasi unde nesciunt adipisci numquam iure explicabo, suscipit distinctio maxime consectetur corrupti voluptates, aliquid, sit consequatur!"
                />
 
                <Card 
-                img="https://sm.mashable.com/t/mashable_in/photo/default/asus-rog-strix-scar-g15-review_8wpm.2496.jpg"
+                img="./4.png"
                 heading="My Dummy Text"
                 para1="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro blanditiis cum alias voluptate fugiat ea, repellat omnis sequi accusamus officia nemo praesentium quibusdam quo illum sint id explicabo repellendus, quia eaque ex? Maxime vitae officia aliquam, nisi perspiciatis id quasi corrupti, perferendis ducimus vero ipsa!lorem10 Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, tenetur!"
                 para2="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quasi unde nesciunt adipisci numquam iure explicabo, suscipit distinctio maxime consectetur corrupti voluptates, aliquid, sit consequatur!"
                 />
 
                <Card2
-               img="https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2019-10/lays.jpg"
+               img="./bloodDoor.png"
                heading="My Dummy Text"
                para1="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro blanditiis cum alias voluptate fugiat ea, repellat omnis sequi accusamus officia nemo praesentium quibusdam quo illum sint id explicabo repellendus, quia eaque ex? Maxime vitae officia aliquam, nisi perspiciatis id quasi corrupti, perferendis ducimus vero ipsa!lorem10 Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, tenetur!"
                para2="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quasi unde nesciunt adipisci numquam iure explicabo, suscipit distinctio maxime consectetur corrupti voluptates, aliquid, sit consequatur!"
